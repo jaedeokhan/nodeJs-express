3db714f97be43e3c1dfe5bce0d8a253a41d405fa
jaedeokhan <jaedeokhan@gmail.com>
2022-08-20 19:03:47 +0900

    figlet

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 console.log("hello nodejs!");
+
+var figlet = require('figlet');
+
+figlet('DeokDuck', function(err, data) {
+    if (err) {
+        console.log('Something went wrong...');
+        console.dir(err);
+        return;
+    }
+    console.log(data)
+});
