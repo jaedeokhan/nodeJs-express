d7b90a0bb2f0edf3e990f2c98e8e67380f3ed512
jaedeokhan <jaedeokhan@gmail.com>
2022-08-21 14:21:44 +0900

    express get, post

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
-console.log("hello nodejs!");
+const express = require('express')
+const app = express()
+const port = 3000
 
-var figlet = require('figlet');
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 
-figlet('DeokDuck', function(err, data) {
-    if (err) {
-        console.log('Something went wrong...');
-        console.dir(err);
-        return;
-    }
-    console.log(data)
-});
+app.get('/user/:id', (req, res) => {
+  const p = req.params;
+  console.log(p);
+  const q = req.query;
+  console.log(q);
+
+  res.send({'message' : 'Hello World!'});
+})
+
+app.use(express.json());
+app.post('/user/:id', (req, res) => {
+  const p = req.params;
+  console.log(p);
+  const b = req.body;
+  console.log(b);
+
+  res.send({'message' : 'Hello World!'});
+})
+
+app.listen(port, () => {
+  console.log(`listening on port ${port}`)
+})
